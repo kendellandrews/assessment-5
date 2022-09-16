76d520196dbdae3f60333dd909e8d41c4c4e4c1b
Kendell Andrews <kendellandrews2@gmail.com>
2022-09-16 12:09:05 -0700

    initial

---
+++ assessment-data/travel-journal/server/controller.js
@@ -0,0 +1,269 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env 
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+  })
+
+
+module.exports = {
+    seed: (req, res) => {
+        sequelize.query(`
+            drop table if exists cities;
+            drop table if exists countries;
+
+            create table countries (
+                country_id serial primary key, 
+                name varchar
+            );
+
+            create table cities (
+                city_id serial primary key, 
+                name varchar(40),
+                rating integer,
+                country_id integer REFERENCE countries_id
+
+            );
+
+            insert into countries (name)
+            values ('Afghanistan'),
+            ('Albania'),
+            ('Algeria'),
+            ('Andorra'),
+            ('Angola'),
+            ('Antigua and Barbuda'),
+            ('Argentina'),
+            ('Armenia'),
+            ('Australia'),
+            ('Austria'),
+            ('Azerbaijan'),
+            ('Bahamas'),
+            ('Bahrain'),
+            ('Bangladesh'),
+            ('Barbados'),
+            ('Belarus'),
+            ('Belgium'),
+            ('Belize'),
+            ('Benin'),
+            ('Bhutan'),
+            ('Bolivia'),
+            ('Bosnia and Herzegovina'),
+            ('Botswana'),
+            ('Brazil'),
+            ('Brunei'),
+            ('Bulgaria'),
+            ('Burkina Faso'),
+            ('Burundi'),
+            ('Côte d''Ivoire'),
+            ('Cabo Verde'),
+            ('Cambodia'),
+            ('Cameroon'),
+            ('Canada'),
+            ('Central African Republic'),
+            ('Chad'),
+            ('Chile'),
+            ('China'),
+            ('Colombia'),
+            ('Comoros'),
+            ('Congo'),
+            ('Costa Rica'),
+            ('Croatia'),
+            ('Cuba'),
+            ('Cyprus'),
+            ('Czech Republic'),
+            ('Democratic Republic of the Congo'),
+            ('Denmark'),
+            ('Djibouti'),
+            ('Dominica'),
+            ('Dominican Republic'),
+            ('Ecuador'),
+            ('Egypt'),
+            ('El Salvador'),
+            ('Equatorial Guinea'),
+            ('Eritrea'),
+            ('Estonia'),
+            ('Eswatini'),
+            ('Ethiopia'),
+            ('Fiji'),
+            ('Finland'),
+            ('France'),
+            ('Gabon'),
+            ('Gambia'),
+            ('Georgia'),
+            ('Germany'),
+            ('Ghana'),
+            ('Greece'),
+            ('Grenada'),
+            ('Guatemala'),
+            ('Guinea'),
+            ('Guinea-Bissau'),
+            ('Guyana'),
+            ('Haiti'),
+            ('Holy See'),
+            ('Honduras'),
+            ('Hungary'),
+            ('Iceland'),
+            ('India'),
+            ('Indonesia'),
+            ('Iran'),
+            ('Iraq'),
+            ('Ireland'),
+            ('Israel'),
+            ('Italy'),
+            ('Jamaica'),
+            ('Japan'),
+            ('Jordan'),
+            ('Kazakhstan'),
+            ('Kenya'),
+            ('Kiribati'),
+            ('Kuwait'),
+            ('Kyrgyzstan'),
+            ('Laos'),
+            ('Latvia'),
+            ('Lebanon'),
+            ('Lesotho'),
+            ('Liberia'),
+            ('Libya'),
+            ('Liechtenstein'),
+            ('Lithuania'),
+            ('Luxembourg'),
+            ('Madagascar'),
+            ('Malawi'),
+            ('Malaysia'),
+            ('Maldives'),
+            ('Mali'),
+            ('Malta'),
+            ('Marshall Islands'),
+            ('Mauritania'),
+            ('Mauritius'),
+            ('Mexico'),
+            ('Micronesia'),
+            ('Moldova'),
+            ('Monaco'),
+            ('Mongolia'),
+            ('Montenegro'),
+            ('Morocco'),
+            ('Mozambique'),
+            ('Myanmar'),
+            ('Namibia'),
+            ('Nauru'),
+            ('Nepal'),
+            ('Netherlands'),
+            ('New Zealand'),
+            ('Nicaragua'),
+            ('Niger'),
+            ('Nigeria'),
+            ('North Korea'),
+            ('North Macedonia'),
+            ('Norway'),
+            ('Oman'),
+            ('Pakistan'),
+            ('Palau'),
+            ('Palestine State'),
+            ('Panama'),
+            ('Papua New Guinea'),
+            ('Paraguay'),
+            ('Peru'),
+            ('Philippines'),
+            ('Poland'),
+            ('Portugal'),
+            ('Qatar'),
+            ('Romania'),
+            ('Russia'),
+            ('Rwanda'),
+            ('Saint Kitts and Nevis'),
+            ('Saint Lucia'),
+            ('Saint Vincent and the Grenadines'),
+            ('Samoa'),
+            ('San Marino'),
+            ('Sao Tome and Principe'),
+            ('Saudi Arabia'),
+            ('Senegal'),
+            ('Serbia'),
+            ('Seychelles'),
+            ('Sierra Leone'),
+            ('Singapore'),
+            ('Slovakia'),
+            ('Slovenia'),
+            ('Solomon Islands'),
+            ('Somalia'),
+            ('South Africa'),
+            ('South Korea'),
+            ('South Sudan'),
+            ('Spain'),
+            ('Sri Lanka'),
+            ('Sudan'),
+            ('Suriname'),
+            ('Sweden'),
+            ('Switzerland'),
+            ('Syria'),
+            ('Tajikistan'),
+            ('Tanzania'),
+            ('Thailand'),
+            ('Timor-Leste'),
+            ('Togo'),
+            ('Tonga'),
+            ('Trinidad and Tobago'),
+            ('Tunisia'),
+            ('Turkey'),
+            ('Turkmenistan'),
+            ('Tuvalu'),
+            ('Uganda'),
+            ('Ukraine'),
+            ('United Arab Emirates'),
+            ('United Kingdom'),
+            ('United States of America'),
+            ('Uruguay'),
+            ('Uzbekistan'),
+            ('Vanuatu'),
+            ('Venezuela'),
+            ('Vietnam'),
+            ('Yemen'),
+            ('Zambia'),
+            ('Zimbabwe');
+        `).then(() => {
+            console.log('DB seeded!')
+            res.sendStatus(200)
+        }).catch(err => console.log('error seeding DB', err))
+    },
+
+    getCountries: (req, res) => {
+        const {name, country_id} = req.body
+        sequelize.query(`select * from countries`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => res.status(500).send(err))
+    },
+
+
+    // In controller.js, write a new function called createCity
+    // Using sequelize.query query your database to insert some data into your table. A name, a rating, and a countryId will be sent on the req.body. Write an insert statement that adds those into the database. (Remember to use a template string for this, and feel free to destructure the values from req.body if you’d like).
+    // Handle the promise with .then() passing in a callback: dbRes => res.status(200).send(dbRes[0]) (you can also add a .catch)
+    
+
+    createCity: (req, res) => {
+        const {name, rating, countryId} = req.body
+        sequelize.query(`insert into countries (name, rating, country_id)`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => res.status(500).send(err))
+    },
+
+
+//  In controller.js, write a new function called getCities
+// Using sequelize.query query your database for columns from both the cities and countries tables. cities: city_id, name (alias ‘city), rating. countries: country_id, name (alias ‘country’). Make sure to spellcheck the aliases as well as the column names. Join the tables where country_id is equal.
+// Handle the promise with .then() passing in a callback: dbRes => res.status(200).send(dbRes[0]) (you can also add a .catch)
+    
+    createCity: (req, res) => {
+        
+    }
+
+
+
+
+}
